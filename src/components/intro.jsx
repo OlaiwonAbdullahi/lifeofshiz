939b2e696e3f3e0f7efd04d12d93c8dc87b05ed1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-11 18:15:40 +0100

    Refactor Intro component for improved styling and structure

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,8 +2,8 @@ import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 function Intro() {
   return (
     <section className="text-center bg-[#e3dcdc] /*text-[#0d0c0d]*/ text-[#6b4c2d] flex flex-col items-center p-[30px]">
-      <h1 className="mb-[15px] text-3xl font-bold">Who is Shiz?</h1>
-      <p className="text-xl leading-relaxed">
+      <h1 className="mb-[15px] text-3xl font-bold font-ubuntu">Who is Shiz?</h1>
+      <p className="text-base leading-relaxed font-Montserrat">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
         cupiditate illum consequuntur doloribus adipisci aliquam repellendus
         temporibus dolor molestiae itaque voluptatem aut explicabo neque
@@ -12,12 +12,11 @@ function Intro() {
         voluptatum nobis? Corrupti enim quos sapiente ducimus, quis laboriosam
         adipisci laudantium omnis unde accusamus expedita tempore sed quo.
       </p>
-      <a
-        href="#"
-        className="text-[#601115] text-xl mt-[10px] border-b-[2px] flex items-center"
-      >
-        More About Shizrae <MdOutlineKeyboardDoubleArrowRight />
-      </a>
+      <button className="mt-[20px] bg-[#6b4c2d] flex items-center  text-[#e3dcdc] px-[20px] py-[10px] rounded-full font-Montserrat hover:bg-[#695f4d] transition-all duration-300">
+        <a href="#" className="text-sm   flex items-center font-ubuntu">
+          More About Shizrae <MdOutlineKeyboardDoubleArrowRight />
+        </a>
+      </button>
     </section>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "../components/navbar";
 import Hero from "../components/hero";
 import Intro from "../components/intro";
+import SocialBar from "../components/socialbar";
+import Photo from "../components/photo";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Nav />
       <Hero />
       <Intro />
+      <SocialBar />
+      <Photo />
     </div>
   );
 };
