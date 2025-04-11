acac74bda6e64681659048294db276651a7fb3ee
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-11 20:13:45 +0100

    Add FunFacts component and integrate it into the About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Journey from "../components/journey";
 import AboutHero from "../components/aboutHero";
+import FunFacts from "../components/funFacts";
 
 const About = () => {
   return (
     <div>
       <AboutHero />
       <Journey />
+      <FunFacts />
     </div>
   );
 };
