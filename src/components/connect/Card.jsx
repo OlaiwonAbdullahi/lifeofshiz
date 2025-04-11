28635729cd4bdbcb7dd22e875648a31fa805dc46
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-11 22:32:54 +0100

    Refactor Card, SmallCard, and SmallCardtwo components for improved structure and styling; enhance social links and form functionality

--- src/components/connect/Card.jsx
+++ src/components/connect/Card.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import SmallCard from './SmallCard'
-import SmallCardtwo from './SmallCardtwo'
+import React from "react";
+import SmallCard from "./SmallCard";
+import SmallCardtwo from "./SmallCardtwo";
 
 function Card() {
   return (
-    <div className='relative w-[70%] bg-white mx-auto mt-[50px] shadow-lg flex items-start h-[500px]'>
-        <h1 className='w-1/2 mt-[25px] ml-[25px] text-4xl font-bold text-[#695f4d]'>Connect with me</h1>
-        <img className='w-1/2 h-full object-cover' src="/images/collageimg1.jpg" alt="cardimg" />
-        <SmallCard />
-        <SmallCardtwo />
+    <div className="relative w-[70%] bg-white mx-auto mt-[30px]  shadow-lg flex items-start h-[500px]">
+      <h1 className="w-1/2 mt-[25px] ml-[25px] text-4xl font-bold text-[#695f4d] font-ubuntu">
+        Connect with me
+      </h1>
+      <img
+        className="w-1/2 h-full object-cover"
+        src="/images/collageimg1.jpg"
+        alt="cardimg"
+      />
+      <SmallCard />
+      <SmallCardtwo />
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
