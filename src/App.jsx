9a2278d3e35d97908df381e4bedc4d4c07898163
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-11 20:28:21 +0100

    Refactor import statements in App.jsx for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import React from "react";
 
 import Home from "./pages/Home";
 import Nav from "./components/navbar";
-import Home from "./pages/home";
 
 import About from "./pages/about";
-import Nav from "./components/navbar";
 import Footer from "./components/footer";
 
 const App = () => {
   return (
     <div className="flex flex-col w-full">
-       <Router>
+      <Router>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
