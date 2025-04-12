a88e130e20513338192ca20572f581cb43c4e37e
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-12 10:34:07 +0100

    Refactor AdviceBookings component: implement form handling, validation, and success/error messages; enhance SmallCard component styling

--- src/components/connect/SmallCard.jsx
+++ src/components/connect/SmallCard.jsx
@@ -7,7 +7,7 @@ import {
   PiTwitterLogo,
   PiYoutubeLogo,
 } from "react-icons/pi";
-import AdviceBookings from "./adviceBookings";
+import AdviceBookings from "./adviceBookings"; // Capitalized import name for consistency
 
 const socialLinks = [
   { icon: <PiTiktokLogoLight className="w-6 h-6" />, label: "Tiktok" },
@@ -20,9 +20,10 @@ const socialLinks = [
 
 function SmallCard() {
   const [openAdviceBooking, setOpenAdviceBooking] = useState(false);
+
   return (
-    <div className="absolute left-[5%] bottom-5 w-[90%] sm:w-[70%] md:w-[40%] h-[70%] p-6  flex flex-col items-center justify-start">
-      <div className="">
+    <div className="absolute left-[5%] bottom-5 w-[90%] sm:w-[70%] md:w-[40%] h-[70%] p-6 flex flex-col items-center justify-start">
+      <div className="w-full">
         <div className="flex flex-wrap justify-between w-full gap-y-3">
           {socialLinks.map((item, index) => (
             <div
@@ -38,7 +39,7 @@ function SmallCard() {
         </div>
       </div>
       <button
-        className="  mt-6 flex items-center gap-2.5 cursor-pointer bg-[#6b4c2d] text-[#e3dcdc] px-6 py-3 rounded-md text-center justify-center font-Montserrat hover:bg-[#695f4d] transition-all duration-300"
+        className="mt-6 flex items-center gap-2.5 cursor-pointer bg-[#6b4c2d] text-[#e3dcdc] px-6 py-3 rounded-md font-Montserrat hover:bg-[#695f4d] transition-all duration-300"
         onClick={() => setOpenAdviceBooking(!openAdviceBooking)}
       >
         Get Advice
