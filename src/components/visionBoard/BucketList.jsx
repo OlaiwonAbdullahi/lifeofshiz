6737992d59187d085807ebf14ea514b88ef7d684
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-13 00:33:24 +0100

    feat: refactor Left and BucketList components for improved structure and styling

--- src/components/visionBoard/BucketList.jsx
+++ src/components/visionBoard/BucketList.jsx
@@ -1,42 +1,29 @@
 import React from "react";
+
 const bucketList = [
-  { title: "Smell a flower", imgLink: "/public/images/collageimg1.jpg" },
-  { title: "Smell a flower", imgLink: "/public/images/collageimg1.jpg" },
-  { title: "Smell a flower", imgLink: "/public/images/collageimg1.jpg" },
+  { title: "Smell a flower", imgLink: "/images/collageimg1.jpg" },
+  { title: "Climb a mountain", imgLink: "/images/collageimg1.jpg" },
+  { title: "Skydive", imgLink: "/images/collageimg1.jpg" },
 ];
+
 function BucketList() {
   return (
-    <section className="bg-[#e3dcdc] p-[30px]">
-      <h1 className="text-3xl text-[#e3dcdc] mb-[20px] ml-[20px]">
+    <section className="bg-[#e3dcdc] p-[30px] text-[#6b4c2d]">
+      <h1 className="text-3xl mb-[20px] ml-[20px] font-bold">
         Shizrae's Bucket List
       </h1>
       <div className="bucketListgrid">
-        {bucketList.map((item, index) => {
-          return (
-            <div
-              className="bg-[#601115]  cursor-pointer rounded-2xl"
-              key={index}
-            >
-              <img
-                src={item.imgLink}
-                alt={item.title}
-                className=" rounded-t-2xl"
-              />
-              <h2 className="text-xl p-[20px] text-[#e3dcdc]">{item.title}</h2>
-            </div>
-          );
-        })}
+        {bucketList.map((item, index) => (
+          <div className="bg-white cursor-pointer rounded-[15px]" key={index}>
+            <img
+              className="rounded-tl-[15px] rounded-tr-[15px] w-full"
+              src={item.imgLink}
+              alt={item.title}
+            />
+            <h2 className="text-xl p-[20px]">{item.title}</h2>
+          </div>
+        ))}
       </div>
-    <section className='bg-[#e3dcdc] p-[30px] text-[#6b4c2d]'>
-        <h1 className='text-3xl mb-[20px] ml-[20px] font-bold'>Shizrae's Bucket List</h1>
-        <div className='bucketListgrid'>
-            {bucketList.map((item, index)=>{
-                return <div className='bg-white cursor-pointer rounded-[15px]' key={index}>
-                    <img className='rounded-tl-[15px] rounded-tr-[15px]' src={item.imgLink} alt={item.title} />
-                    <h2 className='text-xl p-[20px]'>{item.title}</h2>
-                </div>
-            })}
-        </div>
     </section>
   );
 }
