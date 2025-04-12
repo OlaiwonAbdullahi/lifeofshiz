fbc6fcebb35d0eacfb5600fd1c26332deb3ec22d
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-12 09:57:30 +0100

    Add AdviceBookings component and integrate with SmallCard; implement advice booking functionality

--- src/components/connect/SmallCard.jsx
+++ src/components/connect/SmallCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   PiFacebookLogo,
   PiInstagramLogo,
@@ -7,6 +7,7 @@ import {
   PiTwitterLogo,
   PiYoutubeLogo,
 } from "react-icons/pi";
+import AdviceBookings from "./adviceBookings";
 
 const socialLinks = [
   { icon: <PiTiktokLogoLight className="w-6 h-6" />, label: "Tiktok" },
@@ -18,20 +19,31 @@ const socialLinks = [
 ];
 
 function SmallCard() {
+  const [openAdviceBooking, setOpenAdviceBooking] = useState(false);
   return (
-
     <div className="absolute left-[5%] bottom-5 w-[90%] sm:w-[70%] md:w-[40%] h-[70%] p-6  flex flex-col items-center justify-start">
-      <div className="flex flex-wrap justify-between w-full gap-y-3">
-        {socialLinks.map((item, index) => (
-          <div
-            key={index}
-            className="w-1/2 flex items-center gap-2 cursor-pointer"
-          >
-            {item.icon}
-            <span className="font-Montserrat text-sm text-[#333]">Shizrae</span>
-          </div>
-        ))}
+      <div className="">
+        <div className="flex flex-wrap justify-between w-full gap-y-3">
+          {socialLinks.map((item, index) => (
+            <div
+              key={index}
+              className="w-1/2 flex items-center gap-2 cursor-pointer"
+            >
+              {item.icon}
+              <span className="font-Montserrat text-sm text-[#333]">
+                Shizrae
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
+      <button
+        className="  mt-6 flex items-center gap-2.5 cursor-pointer bg-[#6b4c2d] text-[#e3dcdc] px-6 py-3 rounded-md text-center justify-center font-Montserrat hover:bg-[#695f4d] transition-all duration-300"
+        onClick={() => setOpenAdviceBooking(!openAdviceBooking)}
+      >
+        Get Advice
+      </button>
+      {openAdviceBooking && <AdviceBookings />}
     </div>
   );
 }
