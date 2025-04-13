4e4edf7e007c5c7cae62ddc5e242fd5962f00561
Oyedeji Uzaymah Adeyemi <oyedejiuzaymah@gmail.com>
2025-04-13 08:33:02 +0100

    fixed-connectcard-ish

--- src/components/connect/Card.jsx
+++ src/components/connect/Card.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import SmallCard from "./SmallCard";
 import SmallCardtwo from "./SmallCardtwo";
+import AdviceBookings from "./adviceBookings";
 
 function Card() {
+  const [switchForm, setSwitchForm] = useState(false)
   return (
-    <div className="relative w-[70%] bg-white mx-auto mt-[30px]  shadow-lg flex items-start h-[500px]">
-      <h1 className="w-1/2 mt-[25px] ml-[25px] text-4xl font-bold text-[#695f4d] font-ubuntu">
+    <div className="relative w-[95%] md:w-[70%] bg-white mx-auto mt-[30px]  shadow-lg flex items-start h-[500px]">
+      <h1 className="w-1/2 mt-[25px] ml-[25px] md:text-4xl font-bold text-[#695f4d] font-ubuntu">
         Connect with me
       </h1>
       <img
@@ -13,8 +15,8 @@ function Card() {
         src="/images/collageimg1.jpg"
         alt="cardimg"
       />
-      <SmallCard />
-      <SmallCardtwo />
+      <SmallCard switchForm={switchForm} setSwitchForm={setSwitchForm} />
+      {!switchForm ? <SmallCardtwo /> : <AdviceBookings />}
     </div>
   );
 }
