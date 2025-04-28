c72a3df52e2a396602d286d2e3d36bffb09abe0e
Codebox124 <rajcodes733@gmail.com>
2025-04-28 23:30:09 +0100

    updatdd

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import hero from "../assets/about_cover.png";
+import hero from "../assets/home_cover.png";
 
 function Hero() {
   return (
-    <section className="  md:my-[10vh] my-[9vh]  ">
+    <section className="   md:my-[10vh]  ">
+      <p>living learning, and documenting in between" to “Your corporate cowgirl travel bestie- conquering boardrooms and bucket lists.</p>
       <img src={hero} alt="" className=" h-[50vh]  md:h-[88vh] w-full" />
     </section>
   );
