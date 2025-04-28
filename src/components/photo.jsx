dd7c6f63cac799c3bc299932cae542ec9b239ace
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-04-28 23:13:19 +0100

    some stuff

--- src/components/photo.jsx
+++ src/components/photo.jsx
@@ -1,51 +1,86 @@
-function Photo() {
+// Photo.jsx
+import Slider from "react-slick";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Custom Next Arrow
+function NextArrow({ onClick }) {
   return (
-    <section className="photos flex flex-col md:flex-row w-full gap-3 p-5 bg-[#f1ebdd] items-stretch">
-      {/* Left large image */}
-      <a href="#" className="w-full md:w-1/2 h-[400px] md:h-auto">
-        <img
-          className="w-full h-full object-cover object-top rounded-md cursor-pointer"
-          src="/images/collageimg1.jpg"
-          alt="collageimg"
-        />
-      </a>
-
-      {/* Right collage */}
-      <div className="flex flex-col w-full md:w-1/2 gap-3 h-full">
-        <a href="#" className="w-full h-full">
-          <img
-            className="w-full h-full object-cover object-center rounded-md cursor-pointer"
-            src="/images/collageimg2.jpg"
-            alt="collageimg"
-          />
-        </a>
-
-        <div className="flex gap-3 h-1/2">
-          <a href="#" className="w-1/2 h-full">
-            <img
-              className="w-full h-full object-cover object-top rounded-md cursor-pointer"
-              src="/images/collageimg3.jpg"
-              alt="collageimg"
-            />
-          </a>
-          <a href="#" className="w-1/2 h-full">
-            <img
-              className="w-full h-full object-cover object-top rounded-md cursor-pointer"
-              src="/images/collageimg4.jpg"
-              alt="collageimg"
-            />
-          </a>
-        </div>
-        <a href="#" className="w-full h-full">
-          <img
-            className="w-full h-full object-cover object-center rounded-md cursor-pointer"
-            src="/images/collageimg2.jpg"
-            alt="collageimg"
-          />
-        </a>
-      </div>
-    </section>
+    <div
+      className="absolute top-1/2 -translate-y-1/2 right-3 w-10 h-10 bg-[#bf6d45] rounded-full flex items-center justify-center cursor-pointer hover:bg-[#a4542e] z-10"
+      onClick={onClick}
+    >
+      <FaArrowRight className="text-white text-xl" />
+    </div>
   );
 }
 
-export default Photo;
+// Custom Previous Arrow
+function PrevArrow({ onClick }) {
+  return (
+    <div
+      className="absolute top-1/2 -translate-y-1/2 left-3 w-10 h-10 bg-[#bf6d45] rounded-full flex items-center justify-center cursor-pointer hover:bg-[#a4542e] z-10"
+      onClick={onClick}
+    >
+      <FaArrowLeft className="text-white text-xl" />
+    </div>
+  );
+}
+
+export default function Photo() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <section className="w-10/12 mx-auto p-5 bg-[#f1ebdd]">
+      <Slider {...settings}>
+        <div>
+          <img
+            src="/images/collageimg1.jpg"
+            alt="Collage 1"
+            className="w-full h-[500px] object-cover rounded-md"
+          />
+        </div>
+
+        <div>
+          <img
+            src="/images/collageimg2.jpg"
+            alt="Collage 2"
+            className="w-full h-[500px] object-cover rounded-md"
+          />
+        </div>
+
+        <div className="">
+          <img
+            src="/images/collageimg3.jpg"
+            alt="Collage 3"
+            className="w-full h-[500px] object-cover rounded-md"
+          />
+        </div>
+        <div className="">
+          <img
+            src="/images/collageimg4.jpg"
+            alt="Collage 4"
+            className="w-full h-[500px] object-cover rounded-md"
+          />
+        </div>
+
+        <div>
+          <img
+            src="/images/collageimg2.jpg"
+            alt="Collage 5"
+            className="w-full h-[500px] object-cover rounded-md"
+          />
+        </div>
+      </Slider>
+    </section>
+  );
+}
